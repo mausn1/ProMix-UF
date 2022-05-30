71d71673ae754ff74b1db6b0f563b29d11e2a725
maus <75853569+mausn1@users.noreply.github.com>
2022-05-30 09:44:39 +0200

    Changed some basic stuff and added links

--- account/mypage/settings/edit-privacy.php
+++ account/mypage/settings/edit-privacy.php
@@ -12,8 +12,8 @@ if (!$_SESSION['username']) {
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
     />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="/uf.png" />
-
     <link rel="stylesheet" href="/style.css" />
     <title>Mina sidor - ProMix</title>
   </head>
